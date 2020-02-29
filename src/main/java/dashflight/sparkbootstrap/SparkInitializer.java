866566d6940a1ad02629229858c0969d34c50bf3
jbeck18 <j.beck18@me.com>
2020-02-29 12:37:44 -0500

    Refactored to use new config module

--- src/main/java/dashflight/sparkbootstrap/SparkInitializer.java
+++ src/main/java/dashflight/sparkbootstrap/SparkInitializer.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import net.dashflight.data.RuntimeEnvironment;
+import net.dashflight.data.config.RuntimeEnvironment;
 import schemabuilder.processor.pipelines.parsing.dataloaders.DataLoaderRepository;
 import spark.Spark;
 
